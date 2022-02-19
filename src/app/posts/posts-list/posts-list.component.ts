51b3c12da80fc355824f10fcbbdf3e4c98b39e4e
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-02-20 00:20:40 +0100

    created Selector for Post

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './../../store/app.state';
 
 @Component({
   selector: 'app-posts-list',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
   }
-
+   
 }
